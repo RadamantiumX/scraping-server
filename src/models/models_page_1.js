37fc7d86b7ca2a4c002cca0ab32348d5366d05f9
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-20 18:51:58 -0300

    path change

--- src/models/models_page_1.js
+++ src/models/models_page_1.js
@@ -1,15 +1,12 @@
 import { readJSON } from "../utils/readJson.js";
-// import path from '../phubData/models_page_1.json'
-import fs from 'node:fs'
 
-const modelsPage1 = readJSON('../phubData/models_page_1.json')
+const modelsPage1 = readJSON("../../models_page_1.json")
 
 
 
 export class PhubModel{
     static async getAll(){
         const data = modelsPage1
-
         return data
     }
 }
