95056c3df0f0e4fd481e3458c1442893cd2d23f6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-19 22:03:02 -0300

    testing headers

--- src/controllers/pornhub.controller.js
+++ src/controllers/pornhub.controller.js
@@ -1,15 +1,17 @@
 import { PornHub } from "pornhub.js";
 import { StatusCodes } from "http-status-codes"
 import { HttpsProxyAgent } from "https-proxy-agent";
+import { HttpProxyAgent } from "http-proxy-agent";
 
 export class PornHubController{
     async pornHubApi(req, res, next){
-        
-        try{
-        const proxy = 'https://162.241.204.101:60526'
-        const httpsAgent =  new HttpsProxyAgent(proxy)
+        const agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
+        const header = 'User-Agent'
+        const proxy = 'https://8.219.97.248:80'
+        const httpsAgent = new HttpsProxyAgent(proxy)
+        try{    
         const pornhub = new PornHub()
-         pornhub.setAgent(httpsAgent)
+        pornhub.setHeader(header, agent)
         const models = await pornhub.pornstarList({
             page: 1,
             gender: 'female',
