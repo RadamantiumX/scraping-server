e975402cd58804be53bcf03d06d0e6b88e1eaa2a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-23 15:33:30 -0300

    test some fix

--- src/models/phubModels.js
+++ src/models/phubModels.js
@@ -3,7 +3,7 @@ import { readJSON } from "../../utils.js";
 
 export class PhubModel{
     static async getAll(){
-        const data = readJSON("./data/modelsPages/models.json")
+        const data = readJSON("./data/modelsPages/models-page-1.json")
         return data
     }
 }
